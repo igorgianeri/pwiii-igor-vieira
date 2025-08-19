c7e3af6390c975d25878ee746f4914d6e46d5347
Igor <gianeri.vieira@gmail.com>
2025-08-18 22:22:03 -0300

    Aula de laravel com tailwind e routes

---
+++ aula02/resources/views/login.blade.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    @vite('resources/css/app.css')
+  </head>
+  <body>
+    <h1 class="text-3xl font-bold underline">
+      Hello World!
+    </h1>
+  </body>
+</html>
