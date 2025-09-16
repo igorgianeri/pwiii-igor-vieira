2d68bb45668b5405b99c9aeb4197376089d38763
Igor <gianeri.vieira@gmail.com>
2025-09-15 22:21:23 -0300

    Aula de rotas e deploy

--- aula02/routes/web.php
+++ aula02/routes/web.php
@@ -2,10 +2,5 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-    
-Route::get('/paginateste', function () {
-    return view('teste');
-});
+Route::post('/cliente','App\Http\Controllers\ClienteController@store'); //cadastro
+Route::get('/cliente','App\Http\Controllers\ClienteController@index'); //visualização
